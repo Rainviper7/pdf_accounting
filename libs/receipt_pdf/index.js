539e2baad9af3a4b2a71435bb833072740877b15
Pongsakorn  Moonwang <pongsakorn.mon@gmail.com>
2018-05-30 13:47:56 +0700

    checkpoint 1

--- libs/receipt_pdf/index.js
+++ libs/receipt_pdf/index.js
@@ -24,13 +24,38 @@ module.exports = function (options, callback) {
     var fs = require('fs')
         ;
 
-    var filename = options.filePath,
-        data = options.data,
+    var filename = options.filePath;
+
+    var data = options.data,
         dateStamp = moment(data.date).format("DD/MM/YYYY")
         ;
 
     //-------หน้ารายงาน
-    var docDefinition = drawDocDefinition();
+    // var docDefinition = drawDocDefinition();
+    var companies = []
+    _.forEach(data, function (customer, index) {
+        companies.push(drawDocDefinition(customer))
+    });
+
+
+    var res = []
+    _.forEach(companies, function (contentArray, index) {
+
+
+        _.forEach(contentArray, function (data2, index2) {
+            res.push(data2)
+        });
+
+    });
+
+    var docDefinition = {
+        content: companies,
+        pageSize: 'A4',
+        pageOrientation: 'portrait',
+        // [left, top, right, bottom] or [horizontal, vertical] or just a number for equal margins
+        pageMargins: [50, 50, 50, 50]
+    }
+    // var docDefinition = drawDoc2();
 
     //----------main
     buildReport(docDefinition);
@@ -48,76 +73,73 @@ module.exports = function (options, callback) {
         }, 1500);
     }
 
-    function drawDocDefinition() {
-        return {
+    function drawDocDefinition(company) {
+        return [
 
-            content: [
-                {
-                    style: C.FONT_STYLES.tableExample,
-                    table: {
-                        widths: [150, 350],
-                        body: drawHeader(),
-                    },
-                    layout: 'noBorders'
+            {
+                style: C.FONT_STYLES.tableExample,
+                table: {
+                    widths: [150, 350],
+                    body: drawHeader(),
                 },
-                blankCell(), blankCell(), blankCell(),
-                blankCell(), blankCell(),
-                {
-                    text: 'ใบเสร็จรับเงิน / ใบกำกับภาษี',
-                    style: C.FONT_STYLES.HEADER,
-                    alignment: 'center'
+                layout: 'noBorders'
+            },
+            blankCell(), blankCell(), blankCell(),
+            blankCell(), blankCell(),
+            {
+                text: 'ใบเสร็จรับเงิน / ใบกำกับภาษี',
+                style: C.FONT_STYLES.HEADER,
+                alignment: 'center'
+            },
+            NewLine(2),
+            {
+                text: 'Customer',
+                bold: true
+            }, {
+                style: C.TABLE_STYLES.TABLE_EXAMPLE,
+                table: {
+                    widths: [375, 100],
+                    body: drawCustomerInfo(company)
                 },
-                NewLine(2),
-                {
-                    text: 'Customer',
-                    bold: true
-                }, {
-                    style: C.TABLE_STYLES.TABLE_EXAMPLE,
-                    table: {
-                        widths: [375, 100],
-                        body: drawCustomerInfo(data)
-                    },
-                    layout: 'noBorders'
+                layout: 'noBorders'
 
+            },
+            NewLine(2)
+            ,
+            //------------------itemList
+            {
+                style: [C.TABLE_STYLES.TABLE_EXAMPLE, C.FONT_STYLES.NORMAL],
+                table: {
+                    heights: 15,
+                    widths: [150, 50, 100, 50, 100],
+                    body: drawDetail(company.list)
                 },
-                NewLine(2)
-                ,
-                //------------------itemList
-                {
-                    style: [C.TABLE_STYLES.TABLE_EXAMPLE, C.FONT_STYLES.NORMAL],
-                    table: {
-                        heights: 15,
-                        widths: [150, 50, 100, 50, 100],
-                        body: drawDetail(data.list)
-                    },
-                    layout: C.TABLE_STYLES.LAYOUT.THIN_GRAY
+                layout: C.TABLE_STYLES.LAYOUT.THIN_GRAY
+            },
+            NewLine(2),
+            {
+                style: [C.TABLE_STYLES.TABLE_EXAMPLE, C.FONT_STYLES.NORMAL],
+                table: {
+                    widths: [295, 70, 100],
+                    body: drawSummaryChart(company)
                 },
-                NewLine(2),
-                {
-                    style: [C.TABLE_STYLES.TABLE_EXAMPLE, C.FONT_STYLES.NORMAL],
-                    table: {
-                        widths: [295, 70, 100],
-                        body: drawSummaryChart(data)
-                    },
-                    layout: C.TABLE_STYLES.LAYOUT.THIN_GRAY
+                layout: C.TABLE_STYLES.LAYOUT.THIN_GRAY
+            },
+            blankCell(), blankCell(), blankCell(),
+            {
+                style: [C.TABLE_STYLES.TABLE_EXAMPLE, C.FONT_STYLES.NORMAL],
+                table: {
+                    widths: [90, 150, 10, 110, 80],
+                    body: drawFooter()
                 },
-                blankCell(), blankCell(), blankCell(),
-                {
-                    style: [C.TABLE_STYLES.TABLE_EXAMPLE, C.FONT_STYLES.NORMAL],
-                    table: {
-                        widths: [90, 150, 10, 110, 80],
-                        body: drawFooter()
-                    },
-                    layout: 'noBorders'
-
-                }
-            ],
-            pageSize: 'A4',
-            pageOrientation: 'portrait',
-            // [left, top, right, bottom] or [horizontal, vertical] or just a number for equal margins
-            pageMargins: [50, 50, 50, 50]
-
-        }
+                layout: 'noBorders'
+            },
+            {
+                text: '',
+                pageOrientation: 'portrait',
+                pageBreak: 'before'
+            }
+        ]
     }
 
     function drawHeader() {
@@ -282,9 +304,9 @@ module.exports = function (options, callback) {
 
                 },
                 'SUBTOTAL', { alignment: 'right', text: summary.subTotal }],
-            [blankCell(), 'DISCOUNT', { alignment: 'right', text: data.discount }],
-            [blankCell(), 'VAT', { alignment: 'right', text: data.vat }],
-            [blankCell(), 'GRAND TOTAL', { alignment: 'right', text: numberWithComma(data.grandTotal) }]
+            [blankCell(), 'DISCOUNT', { alignment: 'right', text: summary.discount }],
+            [blankCell(), 'VAT', { alignment: 'right', text: summary.vat }],
+            [blankCell(), 'GRAND TOTAL', { alignment: 'right', text: numberWithComma(summary.grandTotal) }]
 
         ]
     }
